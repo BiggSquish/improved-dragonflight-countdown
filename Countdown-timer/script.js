09d1283724d5689d6c5fb30468f838821fb43a6d
Scott Alderman <34538486+Scottaa23@users.noreply.github.com>
2022-07-11 10:15:53 -0700

    Upgraded the countdown visually
    Made the countdown look better and be more responsive than it was before.

---
+++ Countdown-timer/script.js
@@ -0,0 +1,46 @@
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
+
+
+
+
+
+const   dragonFlightRelease = '31 Dec 2022'
+
+function countdown() {
+    
+    const dragonFlightReleaseDate = new Date(dragonFlightRelease);
+    const currentDate = new Date();
+
+    const totalSeconds = (dragonFlightReleaseDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+
+    const hours = Math.floor(totalSeconds /3600) %24;
+
+    const minutes = Math.floor(totalSeconds /60) %60;
+
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysElement.innerHTML = days;
+    hoursElement.innerHTML = formatTime(hours);
+    minutesElement.innerHTML = formatTime(minutes);
+    secondsElement.innerHTML = formatTime(seconds);
+
+    
+
+}
+
+function formatTime(time){
+
+    return time < 10 ? `0${time}` : time;
+}
+
+
+
+// Begin countdown in milliseconds
+countdown();
+
+setInterval(countdown, 1000);
